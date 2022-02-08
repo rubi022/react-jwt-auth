77b4ff833c3dd01e3091681e6c6795335ba8a3a4
gayathri <gayathrijanakis@gmail.com>
2022-02-08 21:50:17 +0400

    my commit

---
+++ src/services/auth-header.js
@@ -0,0 +1,10 @@
+export default function authHeader(){
+    const user=JSON.parse(localStorage.getItem('user'));
+
+    if (user && user.accessToken){
+        return{AuthorizationL: 'Bearer'+ user.accessToken };
+    }
+else{
+    return{};
+}
+}
